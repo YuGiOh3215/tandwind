ff27fdef02a04612fc1ad76fc3ed1cb0c56e7d0c
ERoy <131496746+YuGiOh3215@users.noreply.github.com>
2023-04-27 22:41:05 -0400

    removed printing to small display

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ def on_forever():
     global current_WindSpeed, current_WindDirection_List
     current_WindSpeed = weatherbit.wind_speed() * 3600 / 1000
     current_WindDirection_List = weatherbit.wind_direction()
-    if item:
-        basic.show_string("Sp")
-        basic.show_number(Math.round(current_WindSpeed))
-    else:
-        basic.show_string("Dir")
-        basic.show_string(current_WindDirection_List)
-    serial.write_line("" + str(current_WindSpeed) + "," + current_WindDirection_List)
+#    if item:
+#        basic.show_string("Sp")
+#        basic.show_number(Math.round(current_WindSpeed))
+#    else:
+#        basic.show_string("Dir")
+#        basic.show_string(current_WindDirection_List)
+    serial.write_line("" + str(Math.round(current_WindSpeed)) + "," + current_WindDirection_List)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -13,13 +13,11 @@ basic.forever(function on_forever() {
     
     current_WindSpeed = weatherbit.windSpeed() * 3600 / 1000
     current_WindDirection_List = weatherbit.windDirection()
-    if (item) {
-        basic.showString("Sp")
-        basic.showNumber(Math.round(current_WindSpeed))
-    } else {
-        basic.showString("Dir")
-        basic.showString(current_WindDirection_List)
-    }
-    
-    serial.writeLine("" + ("" + current_WindSpeed) + "," + current_WindDirection_List)
+    //     if item:
+    //         basic.show_string("Sp")
+    //         basic.show_number(Math.round(current_WindSpeed))
+    //     else:
+    //         basic.show_string("Dir")
+    //         basic.show_string(current_WindDirection_List)
+    serial.writeLine("" + ("" + Math.round(current_WindSpeed)) + "," + current_WindDirection_List)
 })
