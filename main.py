37b0a44478b02f0b162ec88710466a97d67e50e0
ERoy <131496746+YuGiOh3215@users.noreply.github.com>
2023-04-27 22:51:22 -0400

    Added temperature

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 current_WindDirection_List = ""
 current_WindSpeed = 0
+tempC = 0
 item = False
 serial.redirect_to_usb()
 weatherbit.start_wind_monitoring()
@@ -18,6 +19,9 @@ Note: If "???" is displayed, direction is unknown!
 
 def on_forever():
     global current_WindSpeed, current_WindDirection_List
+    global tempC
+
+    # -------- wind --------
     current_WindSpeed = weatherbit.wind_speed() * 3600 / 1000
     current_WindDirection_List = weatherbit.wind_direction()
 #    if item:
@@ -26,5 +30,19 @@ def on_forever():
 #    else:
 #        basic.show_string("Dir")
 #        basic.show_string(current_WindDirection_List)
-    serial.write_line("" + str(Math.round(current_WindSpeed)) + "," + current_WindDirection_List)
+
+    # -------- temperature --------
+    tempC = Math.idiv(weatherbit.soil_temperature(), 100)
+#    if item:
+#        basic.show_string("Ground Temp: ")
+#        basic.show_number(tempC)
+#    else:
+#        if tempC < 12:
+#            basic.show_string("Don't Plant!")
+#        else:
+#            basic.show_string("Plant")
+
+    serial.write_line("" + str(Math.round(current_WindSpeed)) + "," + current_WindDirection_List + "," +tempC)
+    basic.pause(1 * 3000)
+    
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 })
 let current_WindDirection_List = ""
 let current_WindSpeed = 0
+let tempC = 0
 let item = false
 serial.redirectToUSB()
 weatherbit.startWindMonitoring()
@@ -11,6 +12,8 @@ item = true
 /** Note: If "???" is displayed, direction is unknown! */
 basic.forever(function on_forever() {
     
+    
+    //  -------- wind --------
     current_WindSpeed = weatherbit.windSpeed() * 3600 / 1000
     current_WindDirection_List = weatherbit.windDirection()
     //     if item:
@@ -19,5 +22,16 @@ basic.forever(function on_forever() {
     //     else:
     //         basic.show_string("Dir")
     //         basic.show_string(current_WindDirection_List)
-    serial.writeLine("" + ("" + Math.round(current_WindSpeed)) + "," + current_WindDirection_List)
+    //  -------- temperature --------
+    tempC = Math.idiv(weatherbit.soilTemperature(), 100)
+    //     if item:
+    //         basic.show_string("Ground Temp: ")
+    //         basic.show_number(tempC)
+    //     else:
+    //         if tempC < 12:
+    //             basic.show_string("Don't Plant!")
+    //         else:
+    //             basic.show_string("Plant")
+    serial.writeLine("" + ("" + Math.round(current_WindSpeed)) + "," + current_WindDirection_List + "," + tempC)
+    basic.pause(1 * 3000)
 })
